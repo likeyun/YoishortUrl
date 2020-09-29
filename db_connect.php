a15f534b08eff71ca8f303c83819ff11f11b2e9b
TANKING <king@likeyunba.com>
2020-09-29 11:01:58 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-	$db_url = "bdm643964684.my3w.com"; // 数据库服务器
-	$db_user = "bdm643964684"; // 数据库账号
-	$db_pwd = "Likeyun2019"; // 数据库密码
-	$db_name = "bdm643964684_db"; // 数据库名
-	$server_path = "http://localhost/dwz/"; //短网址域名和代码所在目录
-	$table = "dwzdb"; // 短网址数据表名
+	$db_url = "xx"; // 数据库服务器
+	$db_user = "xx"; // 数据库账号
+	$db_pwd = "xx"; // 数据库密码
+	$db_name = "xx"; // 数据库名
+	$server_path = "xx"; //短网址域名和代码所在目录
+	$table = "xxx"; // 短网址数据表名
 ?>
